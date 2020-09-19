1b2888ffb332212542aae231beb1b8a647948d07
Mathias Gout <mathias.gout@gmail.com>
2020-09-19 12:11:08 +0200

    group_window + menu_window creation

---
+++ group_window.py
@@ -0,0 +1,15 @@
+""" Group Window part """
+
+import tkinter as tk
+
+
+class GroupWindow:
+    """ Group Window """
+    
+    def __init__(self):
+        
+        self.master = tk.Tk()
+
+        # Main window cuztomization
+
+        self.master.mainloop()
